64a6408f76498f74a121a157449fb2fb87a17d50
root <root@pisolo.melillo.lcl>
2013-11-21 22:12:57 +0100

    First import

---
+++ subdownloader/__init__.py
@@ -0,0 +1,24 @@
+"""
+This file is part of SubDownloaderLite.
+
+    SubDownloaderLite is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    SubDownloaderLite is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+VERSION = (0, 0, 7, 'alpha', 0)
+
+
+def get_version(*args, **kwargs):
+    # Don't litter django/__init__.py with all the get_version stuff.
+    # Only import if it's actually called.
+    from subdownloader.util.version import get_version
+    return get_version(*args, **kwargs)
