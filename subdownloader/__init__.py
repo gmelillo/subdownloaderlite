8e47dcbb891e1c5cd553c72258e6263c012c12da
Gabriel Melillo <gabriel@melillo.me>
2013-11-26 10:43:37 +0100

    typo

--- subdownloader/__init__.py
+++ subdownloader/__init__.py
@@ -18,5 +18,5 @@ VERSION = (0, 1, 0, 'alpha', 0)
 
 
 def get_version(*args, **kwargs):
-    from subdownloader.util.version import get_version
+    from subdownloader.utils.version import get_version
     return get_version(*args, **kwargs)
